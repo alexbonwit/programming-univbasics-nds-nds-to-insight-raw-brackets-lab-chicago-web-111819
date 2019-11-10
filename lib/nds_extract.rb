3a8b34ad38858a18e726775bdf37ee06bcb78e44
Alex Bonwit <alexbonwit@mac.com>
2019-11-10 00:49:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -70,12 +70,12 @@ def directors_totals(nds)
   
   directors_gross = {}
   counter = 0
-  movies_gross = 0
-  
+
   while counter < nds.length do
+    movies_gross = 0
     inner_counter = 0
     while inner_counter < nds[:movies].length do
-      
+      movies_gross += nds[:movies][:worldwide_gross]
       inner_counter += 1
     end
     directors_gross[nds[counter][:name]] = movies_gross
